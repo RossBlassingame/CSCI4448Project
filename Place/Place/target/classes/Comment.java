ce2ce8224661d1dc2ae29d59a074d7d33b42d797
Ross Blassingame <ross.blassingame@colorado.edu>
2017-12-01 13:00:40 -0700

    Added new Place project. That's the one we want to work on. I left the old one 'Place_OLD' in case anybody needed it for reference.

---
+++ Place/Place/target/classes/Comment.java
@@ -0,0 +1,9 @@
+
+public class Comment {
+	
+	// Each comment has a unique commentID.
+	int commentID;
+	// Each comment has some content.
+	String commentContent;
+	
+}
---
+++ Place/Place/target/classes/View.java
@@ -0,0 +1,5 @@
+
+public abstract class View {
+	int Width;
+	int Height;
+}
