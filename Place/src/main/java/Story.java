ea4caf24282a8d7f35bd6f778866a34597cececa
GriffithGNeumark <grne8000@colorado.edu>
2017-12-10 18:55:43 -0700

    Story class hibernate mapping file

--- Place/src/main/java/Story.java
+++ Place/src/main/java/Story.java
@@ -4,13 +4,15 @@ public class Story {
 	
 	// Each story has a unique storyID.
 	int storyID;
+	int accountID;
 	// Each story has some content.
 	String storyContent;
 	// Each story has a list of contents (may be empty).
 	ArrayList<Comment> listOfComments = new ArrayList<Comment>();
 
-	Story(String content){
+	Story(String content, int ID){
 		//TODO generate unique ID
+		accountID = ID;
 		storyContent = content;
 	}
 
